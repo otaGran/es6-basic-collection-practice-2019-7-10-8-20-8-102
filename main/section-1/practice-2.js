bccf36f3a1ac13f27e4d4795b6ada5bd09166354
OtaGran <ujt2h8@gmail.com>
2019-07-11 11:34:17 +0800

    finish practice-2

--- main/section-1/practice-2.js
+++ main/section-1/practice-2.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, collectionB) {
-  console.log(Array.from(new Set(collectionA.filter(v => new Set(collectionB).has(v))));
-  return Array.from(new Set(collectionA.filter(v => new Set(collectionB).has(v)))// [2]
+
+  return Array.from(new Set(collectionA.filter(v => new Set(collectionB[0]).has(v)))// [2]
   )
 
 }
--- main/section-1/practice-3.js
+++ main/section-1/practice-3.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  return Array.from(new Set(collectionA.filter(v => new Set(objectB.value).has(v)));
 }
