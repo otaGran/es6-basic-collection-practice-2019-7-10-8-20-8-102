b54106beffdc0e2440f53981c9937daf7c1dfd5b
beans <ujt2h8@gmail.com>
2019-07-12 05:53:33 +0800

    feat : implement section-2/practice-3

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let collectionB = collection.map(v => v.charAt(0));
+  collection.filter(v => v.length >= 3).map(x =>{
+    collectionB = collectionB.concat(Array((parseInt(x.substr(2)) -1 )  ).fill(x.charAt(0)) );
+    console.log(parseInt(x.substr(2)));
+  });
+  return Array.from(new Set(collectionB)).map(v => {
+    return {"name": v, "summary":
+  });
 }
